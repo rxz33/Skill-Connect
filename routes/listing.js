c08516d46cbe9412ff5f70728ff3e53cc774dd0d
rashi <rashig2344@gmail.com>
2025-11-27 23:13:54 +0530

    commit

--- routes/listing.js
+++ routes/listing.js
@@ -1,53 +1,28 @@
- const express=require("express");
- const router=express.Router();
- const wrapAsync=require("../utility/wrapAsync.js");
- const Listing=require("../models/listing.js");
- const {isLoggedIn,isOwner,validateListing}=require("../middleware.js");
-const listingController=require("../controllers/listings.js");
-const multer = require("multer");
-const{storage}=require("../cloudConfig.js");
-const upload = multer({storage});
-
-
-
-
-
-//Index Route
 router.route("/")
 .get(wrapAsync(listingController.index))
 .post(
     isLoggedIn,
-    upload.single("image"),  // FIX HERE (was listing[image])
+    upload.single("image"),
     validateListing,
     wrapAsync(listingController.createListing)
 );
 
-//new Route
-router.get("/new",isLoggedIn,listingController.renderNewForm);
+// new listing
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-//show Route
-router.route("/:id").get(wrapAsync(listingController.showListing))
-.put(isLoggedIn,isOwner,upload.single("listing[image]"),validateListing,wrapAsync(listingController.updateListing))
-.delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing));
+// show / edit / delete
+router.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put(
+    isLoggedIn,
+    isOwner,
+    upload.single("image"),     // FIXED HERE
+    validateListing,
+    wrapAsync(listingController.updateListing)
+)
+.delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
-//Create Route
-//router.post("/",isLoggedIn,validateListing,wrapAsync(listingController.createListing));  
+// edit page
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
-
-
-
-
-
-//edit Route
-router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm));
-
-//update route
-
-
-//Delete Route
- 
-
- module.exports=router;
-
- 
-  
+module.exports = router;

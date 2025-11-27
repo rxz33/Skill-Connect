bd8b198236e4866924db3574886796ef35cd7924
rashi <rashig2344@gmail.com>
2025-11-27 22:48:16 +0530

    yfh

--- schema.js
+++ schema.js
@@ -1,7 +1,6 @@
-const Joi=require("joi");
+const Joi = require("joi"); // Declare ONLY ONCE
 
-const Joi = require("joi");
-
+// ---------------- LISTING SCHEMA ----------------
 module.exports.listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
@@ -25,17 +24,10 @@ module.exports.listingSchema = Joi.object({
     }).required()
 });
 
+// ---------------- REVIEW SCHEMA ----------------
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
         comment: Joi.string().required()
     }).required()
 });
-
-
-module.exports.reviewSchema=Joi.object({
-    review:Joi.object({
-        rating:Joi.number().required().min(1).max(5),
-        comment:Joi.string().required(),
-    }).required(),
-}); 
